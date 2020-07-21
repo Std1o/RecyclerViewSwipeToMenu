77c8b35c4835fb05c04a93f14e4fdc4006b524ba
Std1o <37378410+Std1o@users.noreply.github.com>
2020-07-21 11:42:02 +0300

    fix recyclerview position

--- app/src/main/java/com/stdio/recyclerviewswipetomenu/MainAdapter.java
+++ app/src/main/java/com/stdio/recyclerviewswipetomenu/MainAdapter.java
@@ -40,6 +40,16 @@ public  class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolde
     }
 
     @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
     public int getItemCount() {
         return modelList.size();
     }
